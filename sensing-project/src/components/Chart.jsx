3a0943a960fb6cb7e3a4de2e57bb008adf8c4129
Bemol MacBook <110437038+bitbemol@users.noreply.github.com>
2022-09-17 01:20:29 -0500

    Working with useState to change with data, still need to implement plotly graph using data

--- sensing-project/src/components/Chart.jsx
+++ sensing-project/src/components/Chart.jsx
@@ -1,9 +1,14 @@
 import '../App.css'
 import Plot from 'react-plotly.js'
+import { useState } from 'react'
 
 function Chart() {
     const sensorsURL = 'http://sensors.local/get-data'
-    let sensorData;
+    const [sensorsData, setSensorsData] = useState(null)
+    const handleGetData = () => {
+        // Get your data manipulation
+        setSensorsData(10) // set data here for the plot
+    }
 
     async function getDataFromSensors() {
         const response = await fetch(sensorsURL)
@@ -13,20 +18,38 @@ function Chart() {
         return response.text()
     }
 
+    if (sensorsData == null) {
+        return (
+            <>
+                <h1>No data</h1>
+                <button onClick={handleGetData}>Calculate Plot</button>
+            </>
+        )
+    }
     return (
-        <Plot
-            data={[
-                {
-                    x: [1, 2, 3],
-                    y: [2, 6, 3],
-                    type: 'scatter',
-                    mode: 'lines+markers',
-                    marker: { color: 'red' },
-                },
-                { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
-            ]}
-            layout={{ width: 320, height: 240, title: 'A Fancy Plot' }}
-        />
+        <>
+            <Plot
+                data={
+                    [
+                        {
+                            x: [1, 2, 3],
+                            y: [2, 6, 3],
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: { color: 'red' },
+                        },
+                        { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
+                    ]}
+                layout={{ width: 520, height: 440, title: 'A Fancy Plot' }
+                }
+            />
+            <div className='card'>
+                <button>Calculate Plot</button>
+                <button>Download Measurements</button>
+            </div>
+
+        </>
+
     )
 
 }
